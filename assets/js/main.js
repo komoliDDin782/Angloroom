7fb22cc26be20f96adb269f5293ee4a165f59b6e
komoliDDin782 <komoliddinkevin@gmail.com>
2026-04-14 18:21:02 +0500

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,10 @@
 const GG1 = document.getElementById('GG1');
-
+const GG2 = document.getElementById('GG2');
 GG1.addEventListener('click', () => {
     // This sends the user directly to GG1.html
-    window.location.href = 'GG1.html'; 
+    window.location.href = './books/GG1.html'; 
+});
+GG2.addEventListener('click', () => {
+    // This sends the user directly to GG1.html
+    window.location.href = './books/GG2.html'; 
 });
